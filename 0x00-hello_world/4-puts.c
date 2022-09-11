e841b402c837615c670663432c1b98a8ab458b07
Barakaholusola <barakaholusola@gmail.com>
2022-09-12 00:45:29 +0100

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main- Serves as the entry point for the program
+*
+*Return:By default returns zerom= if no errors otherwise returns non-zero value
+*/
+int main (void)
+{
+   putd("\"Programmimg is like building a multilingual puzzle");
+   return (0);
+} 
