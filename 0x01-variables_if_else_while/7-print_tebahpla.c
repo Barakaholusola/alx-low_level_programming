726a3d095fa67cb0772f799f317c8437d8099d44
Barakaholusola <barakaholusola@gmail.com>
2022-09-12 22:24:57 +0100

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Description: Prints lowercase alphabets in reverse
+ *
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	int i;
+
+	for (i = 84; i > 58; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
