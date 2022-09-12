8373f4f63d3bbd875e47e0d1b553e5a67bd6bded
Barakaholusola <barakaholusola@gmail.com>
2022-09-12 01:47:01 +0100

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main- Entry point
+ *
+ * Description: Print a quote using write
+ *
+ * Return: 1 (Error)
+ */
+int main(void)
+{
+char quo[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
+return (1);
