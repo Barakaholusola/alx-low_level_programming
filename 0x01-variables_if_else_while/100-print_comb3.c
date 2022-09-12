f601b329278aca4ae812f33f8260a7454bec3b27
Barakaholusola <barakaholusola@gmail.com>
2022-09-12 23:11:55 +0100

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Description: Prints all possible different combinations of two digits
+ *
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i == 8 && j == 9)
+			{	
+				continue;
+			}
+			putchar(',')
+			putchar(' ');
+			continue;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
