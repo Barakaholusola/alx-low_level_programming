db7ed602581b2651129c196f5f72d36d08dd322f
Barakaholusola <barakaholusola@gmail.com>
2022-09-12 23:24:48 +0100

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,15 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 9; i++)
+	for (i = 48; i < 57; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		for (j = i + 1; j < 58; j++)
 		{
+		
 			putchar(i);
 			putchar(j);
-			if (i == 8 && j == 9)
-			{	
+			if (i == 56 && j == 57)
+			{
 				continue;
 			}
 			putchar(',');
