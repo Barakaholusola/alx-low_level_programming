a9c043ed537e1837415d6637c105363d03f42470
Barakaholusola <barakaholusola@gmail.com>
2022-09-12 02:01:05 +0100

    101

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
 *
 * Print out '\"Programming is like building a multilingual puzzle'
 *
-* Return: 1 (Error)
+* Return: 0 (Success)
 */
 int main(void)
 {
-puts("\"Programmimg is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
