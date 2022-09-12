03d2b1297157ba902d951402a90c5f31bed27d2f
Barakaholusola <barakaholusola@gmail.com>
2022-09-12 21:27:13 +0100

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Description: Prints alphabet in lowercase, followed by a new line
+ *
+ * Return: (0);
+ */
+int main(void)
+{
+	int alphabt;
+
+	alphabt = 'a';
+	while (alphabt <= 'z')
+	{
+		if (alphabt == 'e')
+		{
+		}
+		else
+		if (alphabt == 'q')
+		{
+		}
+		else
+		putchar(alphabt);
+		alphabt++;
+	}
+	putchar('\n')
+		return (0);
+}
