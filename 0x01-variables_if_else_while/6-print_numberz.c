f098bcb2c0991c2e27edd3a650c6178b1cbe1339
Barakaholusola <barakaholusola@gmail.com>
2022-09-12 22:03:29 +0100

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main: Entry point
+ *
+ * Description: Prints all single digits of base 10 and a new line
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 24; i <34; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n')
+		return (0);
+}
+
