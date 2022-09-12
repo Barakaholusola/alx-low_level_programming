f60a44fd5bc1a73c0d43363836db638b40c312a3
Barakaholusola <barakaholusola@gmail.com>
2022-09-12 01:29:13 +0100

    6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
-*main- Serves as the entry point for the program
+* main- Serves as the entry point for the program
 *
-*Return:By default returns zero if no errors otherwise returns non-zero value
+* Print out '\"Programming is like building a multilingual puzzle'
+*
+* Return:By default returns zero if no errors otherwise returns non-zero value
 */
 int main(void)
 {
